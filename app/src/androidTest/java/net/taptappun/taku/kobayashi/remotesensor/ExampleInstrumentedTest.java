393a9171ad8386a8f183b3f103c092def9115373
TakuKobayshi <>
2017-09-29 10:31:10 +0900

    first commit

---
+++ app/src/androidTest/java/net/taptappun/taku/kobayashi/remotesensor/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package net.taptappun.taku.kobayashi.remotesensor;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("net.taptappun.taku.kobayashi.remotesensor", appContext.getPackageName());
+    }
+}
